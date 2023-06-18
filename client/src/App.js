aa41ce6c87cb2a7663bd8a8414434849e54da1b8
Sakomm <sakommuri@gmail.com>
2023-06-18 17:14:31 +0530

    fuck css

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [groceryList, setGroceryList] = useState([]);
+  const [output, setOutput] = useState('');
+
+  const handleAddItem = () => {
+    const item = prompt('Enter the item:');
+    if (item) {
+      setGroceryList(prevList => [...prevList, item]);
+    }
+  };
+
+  const handleRemoveItem = () => {
+    const item = prompt('Enter the item to remove:');
+    setGroceryList(prevList => prevList.filter(i => i !== item));
+  };
+
+  const handleClearList = () => {
+    setGroceryList([]);
+  };
+
+  const handleFindCheapestLocation = () => {
+    setOutput('Cheapest location: Supermarket XYZ');
+  };
+
+  const handleFindCoupons = () => {
+    setOutput('Coupons: 10% off on fruits, $2 off on milk');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="panel">
+        <h2>Choose an Action</h2>
+        <button onClick={handleAddItem}>Add Item</button>
+        <button onClick={handleRemoveItem}>Remove Item</button>
+        <button onClick={handleClearList}>Clear List</button>
+        <button onClick={handleFindCheapestLocation}>Find Cheapest Location</button>
+      </div>
+
+      <div className="panel">
+        <h2>Your Grocery List</h2>
+        <ul>
+          {groceryList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="panel">
+        <h2>Output</h2>
+        <div>{output}</div>
+      </div>
     </div>
   );
 }
