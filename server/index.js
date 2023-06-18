865f171f2f3d3b32257fe5648d1ec4f4cd41dd4e
Sakomm <sakommuri@gmail.com>
2023-06-18 14:34:08 +0530

    sync

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// server 
+
+const express = require('express');
+ 
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.listen(port, () => {
+    console.log("Sever listining on port: " + port);
+});
+
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
